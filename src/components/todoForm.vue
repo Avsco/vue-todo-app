03fcfff95ed9ec5475eec719a8c2a7774e2b34e8
Alan Benavides <benacho82@gmail.com>
2021-03-17 12:56:27 -0400

    add: control to form for empty cases

---
+++ src/components/todoForm.vue
@@ -0,0 +1,37 @@
+<template>
+  <form @submit.prevent="createTodo">
+    <input v-model.trim="todo" type="text" placeholder="Create a new todo..." />
+    <span @click="clearInput">X</span>
+  </form>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "todoForm",
+  emits: ["submitTodo"],
+  setup(_, { emit }) {
+    let todo = ref("");
+
+    function clearInput() {
+      todo.value = "";
+    }
+
+    function createTodo() {
+      try {
+        if (todo.value === "") throw new Error("The form must have content");
+
+        // emit("submitTodo", todo.value);
+        clearInput();
+      } catch ({ message }) {
+        console.log(message);
+      }
+    }
+
+    return { todo, clearInput, createTodo };
+  }
+};
+</script>
+
+<style scoped lang="scss"></style>
