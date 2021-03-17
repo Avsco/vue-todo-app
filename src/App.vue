16e36e5b66a30c8ee2bb179e1231aadc48de1bc0
Alan Benavides <benacho82@gmail.com>
2021-03-17 13:51:56 -0400

    refactor: put content of App to todoApp

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
 <template>
   <main>
-    <h1>TODO LIST</h1>
-    <todo-form />
-    <todo-list />
+    <todo-app />
   </main>
 </template>
 
 <script>
-import todoForm from "./components/todoForm.vue";
-import todoList from "./components/todoList.vue";
-import { ref, provide } from "vue";
+import todoApp from "./components/todoApp.vue";
 
 export default {
-  name: "App",
   components: {
-    todoForm,
-    todoList
-  },
-
-  setup() {
-    const todos = ref([]);
-    provide("todos", todos);
-
-    return { todos };
+    todoApp
   }
 };
 </script>
---
+++ src/components/todoApp.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>TODO LIST</h1>
+  <todo-form />
+  <todo-list />
+</template>
+
+<script>
+import todoForm from "./todoForm.vue";
+import todoList from "./todoList.vue";
+import { ref, provide } from "vue";
+
+export default {
+  name: "App",
+  components: {
+    todoForm,
+    todoList
+  },
+
+  setup() {
+    const todos = ref([]);
+    provide("todos", todos);
+  }
+};
+</script>
+
+<style scoped lang="scss"></style>
