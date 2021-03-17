25455e98fc78dfc50dc5afb0031851cda48bbf6c
Alan Benavides <benacho82@gmail.com>
2021-03-17 12:27:44 -0400

    import: inputTodo

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
-  <h1>TODO LIST</h1>
+  <main>
+    <h1>TODO LIST</h1>
+    <input-todo />
+  </main>
 </template>
 
 <script>
-export default {};
+import InputTodo from "./components/InputTodo.vue";
+
+export default {
+  components: {
+    InputTodo
+  },
+
+  setup() {}
+};
 </script>
 
 <style lang="scss"></style>
