55f76078e786dd1cc820f3ad1077b39cd44a5b4d
Alan Benavides <benacho82@gmail.com>
2021-03-17 13:33:32 -0400

    add: todoItem

---
+++ src/components/todoItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <li>{{ todo }}</li>
+</template>
+
+<script>
+export default {
+  name: "todoItem",
+
+  props: {
+    todo: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss"></style>
